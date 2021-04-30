6296f1a5511d6742bf5e653e9c53409358d20d24
joshua <joshua.dueck@gmail.com>
2021-04-29 21:53:01 -0500

    finished day 6

--- day6/aoc_2020_day6_part1.py
+++ day6/aoc_2020_day6_part1.py
@@ -1,6 +1,32 @@
 def main():
     f = open("aoc_2020_day6_input.txt", "r")
 
+    groups = ['']
+    curr_group = 0
+
+    for line in f:
+        if line == '\n':
+            curr_group += 1
+            groups.append('')
+        else:
+            groups[curr_group] += line.strip()
+    
+    total = 0
+    for group in groups:
+        total += count_unique_chars(group)
+
+    print(total)
+
+
+def count_unique_chars(group_string):
+    chars = {}
+    count = 0
+    for char in group_string:
+        if char not in chars:
+            chars[char] = True
+            count += 1
+    
+    return count
 
 if __name__ == "__main__":
     main()
--- day6/aoc_2020_day6_part2.py
+++ day6/aoc_2020_day6_part2.py
@@ -1,6 +1,40 @@
 def main():
     f = open("aoc_2020_day6_input.txt", "r")
 
+    group_members = [[]]
+    curr_group = 0
+
+    for line in f:
+        if line == '\n':
+            curr_group += 1
+            group_members.append([])
+        else:
+            group_members[curr_group].append(line.strip())
+
+    total = 0
+    for group in group_members:
+        total += len(get_shared_answers(group))
+
+    print(total)
+
+
+def get_shared_answers(group_list):
+    chars = {}
+    for member in group_list:
+        for char in member:
+            if char not in chars:
+                chars[char] = 1
+            else:
+                chars[char] += 1
+
+    common_answers = []
+    for char in chars:
+        if chars[char] == len(group_list):
+            common_answers.append(char)
+    
+    return common_answers
+
+
 
 if __name__ == "__main__":
     main()
